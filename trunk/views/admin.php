a7b2c579a3feefdb57b89c712cb36309360c034d
tentblogger <tentblogger@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-05 12:00:28 +0000

    updates for wordpress 3.2
    git-svn-id: https://plugins.svn.wordpress.org/tentblogger-simple-seo-sitemap@404764 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/views/admin.php
+++ trunk/views/admin.php
@@ -2,9 +2,11 @@
 <!-- This plugin is not multisite compatible. -->
 <?php $options = get_option('tentblogger-seo-sitemap'); ?>
 <div class="wrap">
-	<h2>
-		<?php _e("TentBlogger's SEO Sitemap Plugin", "tentblogger-seo-sitemap"); ?>
-	</h2>
+  <div class="icon">
+    <h2>
+      <?php _e("TentBlogger's SEO Sitemap Plugin", "tentblogger-seo-sitemap"); ?>
+    </h2>
+  </div>
 	<?php if(tentblogger_is_private_blog()) { ?>
 		<div class="error">
 			<p>
@@ -12,57 +14,59 @@
 			</p>
 		</div>
 	<?php } else { ?>
-	<div class="tentblogger-postbox postbox">
-		<h3 class="tentblogger-hndle hndle">
-			<span>
-				<?php _e("SEO Sitemap", "tentblogger-seo-sitemap"); ?>
-			</span>
-		</h3>
-		<div class="tentblogger-inside inside">
-			<p class="description">
-				<?php _e('This plugin attempts to streamline the sitemap generation process. Automatic create, gzipping, submission to search engines, and daily execution.', 'tentblogger-seo-sitemap') ?>
-			</p>
-			<fieldset>
-				<span id="last-executed">
-					<strong>
-						<?php _e('Last Successfully Executed: ', 'tentblogger-seo-sitemap'); ?>
-					</strong>
-				</span>
-				<span id="build-date">
-					<?php echo $options['tentblogger-sitemap-date'] ? $options['tentblogger-sitemap-date'] : __("Never", 'tentblogger-seo-sitemap');; ?>
-				</span>
-				<span id="error-message">
-					<?php _e('Your server is not configure to support file-write access. Please see <a href="http://tentblogger.com/seo-sitemap/" target="_blank">this post</a> for troubleshooting instructions.', 'tentblogger-seo-sitemap'); ?>
-				</span>
-				<ul id="tentblogger-seo-sitemap-information" class="tentblogger-seo-sitemap-option">
-					<li>
-						<?php _e('Sitemap successfully generated', 'tentblogger-seo-sitemap'); ?>
-					</li>
-				</ul>
-				<ul id="tentblogger-seo-sitemap-information" class="tentblogger-seo-sitemap-option">
-					<li>
-						<?php _e('Sitemap submitted to Google, Bing, and Yahoo!', 'tentblogger-seo-sitemap'); ?>
-					</li>
-				</ul>
-				<ul id="tentblogger-seo-sitemap-information" class="tentblogger-seo-sitemap-option">
-					<li>
-						<?php _e('Sitemap successfully gzipped.', 'tentblogger-seo-sitemap'); ?>
-					</li>
-				</ul>
-			</fieldset>
-			<input type="submit" id="create_sitemap" name="create_sitemap" value="Build Sitemap Now" />
-			<span id="sitemap_url">	
-				<?php echo get_bloginfo('url') . '/sitemap.xml'; ?>
-			</span>
-		</div>
-			<div class="tentblogger-inside inside">
-				<p>
-					<?php 
-						_e("Read more about the plugin <a href=\"http://tentblogger.com/seo-sitemap\">here</a> and check out <a href=\"http://profiles.wordpress.org/users/tentblogger/\">my other plugins</a>! Feel free to <a href=\"http://twitter.com/tentblogger\" target=\"_blank\">follow me</a> on Twitter!", 'tentblogger-seo-sitemap');
-					?>
-				</p>
-			</fieldset>
-		</div>
+  <div class="postbox-container">
+    <div id="poststuff" class="postbox">
+      <h3 class="hndle">
+        <span>
+          <?php _e("SEO Sitemap", "tentblogger-seo-sitemap"); ?>
+        </span>
+      </h3>
+      <div class="inside">
+        <p class="description">
+          <?php _e('This plugin attempts to streamline the sitemap generation process. Automatic create, gzipping, submission to search engines, and daily execution.', 'tentblogger-seo-sitemap') ?>
+        </p>
+        <fieldset>
+          <span id="last-executed">
+            <strong>
+              <?php _e('Last Successfully Executed: ', 'tentblogger-seo-sitemap'); ?>
+            </strong>
+          </span>
+          <span id="build-date">
+            <?php echo $options['tentblogger-sitemap-date'] ? $options['tentblogger-sitemap-date'] : __("Never", 'tentblogger-seo-sitemap');; ?>
+          </span>
+          <span id="error-message">
+            <?php _e('Your server is not configure to support file-write access. Please see <a href="http://tentblogger.com/seo-sitemap/" target="_blank">this post</a> for troubleshooting instructions.', 'tentblogger-seo-sitemap'); ?>
+          </span>
+          <ul id="tentblogger-seo-sitemap-information" class="tentblogger-seo-sitemap-option">
+            <li>
+              <?php _e('Sitemap successfully generated', 'tentblogger-seo-sitemap'); ?>
+            </li>
+          </ul>
+          <ul id="tentblogger-seo-sitemap-information" class="tentblogger-seo-sitemap-option">
+            <li>
+              <?php _e('Sitemap submitted to Google, Bing, and Yahoo!', 'tentblogger-seo-sitemap'); ?>
+            </li>
+          </ul>
+          <ul id="tentblogger-seo-sitemap-information" class="tentblogger-seo-sitemap-option">
+            <li>
+              <?php _e('Sitemap successfully gzipped.', 'tentblogger-seo-sitemap'); ?>
+            </li>
+          </ul>
+        </fieldset>
+        <input type="submit" id="create_sitemap" name="create_sitemap" class="button-primary" value="Build Sitemap Now" />
+        <span id="sitemap_url">	
+          <?php echo get_bloginfo('url') . '/sitemap.xml'; ?>
+        </span>
+      </div>
+        <div class="inside">
+          <p>
+            <?php 
+              _e("Read more about the plugin <a href=\"http://tentblogger.com/seo-sitemap\">here</a> and check out <a href=\"http://profiles.wordpress.org/users/tentblogger/\">my other plugins</a>! Feel free to <a href=\"http://twitter.com/tentblogger\" target=\"_blank\">follow me</a> on Twitter!", 'tentblogger-seo-sitemap');
+            ?>
+          </p>
+        </fieldset>
+      </div>
+    </div>
 	<?php } // end if ?>
 </div>
 </div>
